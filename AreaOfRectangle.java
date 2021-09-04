6ee27da2f796e102ffa96754f79aeecf88027c3c
manisha-shinde <54435497+manisha-shinde@users.noreply.github.com>
2021-09-04 18:12:27 +0530

    If else Statement

---
+++ AreaOfRectangle.java
@@ -0,0 +1,19 @@
+class AreaOfRectangle 
+{
+	public static void main(String[] args) 
+	{
+		float length=5,breadth=10;
+		float Areaofrect=length*breadth;
+		float Periofrect=2*(length*breadth);
+		System.out.println("Area of Rectangle="+Areaofrect);
+		System.out.println("Perimeter of Rectangle="+Periofrect);
+		if(Areaofrect>Periofrect)
+			System.out.println("Area of Rectangle="+Areaofrect);
+		else
+			System.out.println("Perimeter of Rectangle="+Periofrect);
+
+
+
+
+	}
+}
